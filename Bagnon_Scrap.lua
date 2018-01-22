7612fb95bd8c1757d1572e3ad868c19a295d00ba
Joao Cardoso <contact@jaliborc.com>
2018-01-22 22:23:42 +0000

    Initial commit

---
+++ Bagnon_Scrap.lua
@@ -0,0 +1,66 @@
+--[[
+Copyright 2008-2017 João Cardoso
+Bagnon Scrap is distributed under the terms of the GNU General Public License (Version 3).
+As a special exception, the copyright holders of this addon do not give permission to
+redistribute and/or modify it.
+
+This addon is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the addon. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+
+This file is part of Bagnon Scrap.
+--]]
+
+local Addon = Bagnon
+local ItemSlot = Addon.ItemSlot
+local UpdateBorder = ItemSlot.UpdateBorder
+local r, g, b  = GetItemQualityColor(0)
+
+
+--[[ Ruleset ]]--
+
+Addon.Rules:New('scrap', 'Scrap', 'Interface\\Addons\\Scrap\\Art\\Enabled Box', function(player, bag, slot, bagLink, itemLink)
+	if itemLink and bag and slot then
+		return Scrap:IsJunk(tonumber(strmatch(itemLink, 'item:(%d+)')), bag, slot)
+	end
+end)
+
+
+--[[ Glow and Icon ]]--
+
+function ItemSlot:UpdateBorder()
+	local link = select(7, self:GetInfo())
+	if link then
+		local id = tonumber(strmatch(link, 'item:(%d+)'))
+		local bag, slot
+
+		if not self:IsCached() then
+			bag, slot = self:GetBag(), self:GetID()
+		end
+
+		if Scrap:IsJunk(id, bag, slot) then
+			self:HideBorder()
+			self:SetBorderColor(r, g, b)
+
+			return self.JunkIcon:SetShown(Scrap_Icons)
+		end
+	end
+
+	UpdateBorder(self)
+	self.JunkIcon:Hide()
+end
+
+
+--[[ Update Events ]]--
+
+local function UpdateBags()
+	Addon:UpdateFrames()
+end
+
+hooksecurefunc(Scrap, 'SettingsUpdated', UpdateBags)
+hooksecurefunc(Scrap, 'ToggleJunk', UpdateBags)
+Scrap.HasSpotlight = true
